84dfd99d1f252243524a06bbe31ed6ad14ba69c9
86157 <18334698204@163.com>
2018-12-19 14:24:11 +0800

    github

--- js/js-producer-fund/src/main/java/com/example/jsproducerfund/dao/FundDao.java
+++ js/js-producer-fund/src/main/java/com/example/jsproducerfund/dao/FundDao.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @Mapper
+@Repository
 public interface FundDao {
 
     List<FundInfo> findAll();
