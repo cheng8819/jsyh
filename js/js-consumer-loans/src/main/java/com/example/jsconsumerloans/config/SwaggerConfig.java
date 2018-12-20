3caae7cf9944507bfcdf6a9d8ea9a17cc8fee1a5
Guomingze <44560624+Guomingze@users.noreply.github.com>
2018-12-20 17:47:36 +0800

    swagge

--- js/js-consumer-loans/src/main/java/com/example/jsconsumerloans/config/SwaggerConfig.java
+++ js/js-consumer-loans/src/main/java/com/example/jsconsumerloans/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.jsproducerloans.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.jsconsumerloans.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- js/js-consumer-loans/src/main/java/com/example/jsconsumerloans/controller/LoanapplicationController.java
+++ js/js-consumer-loans/src/main/java/com/example/jsconsumerloans/controller/LoanapplicationController.java
@@ -3,12 +3,16 @@ package com.example.jsconsumerloans.controller;
 import com.example.jsconsumerloans.feign.LoanApplication;
 import com.example.jsconsumerloans.pojo.LoansUserinfo;
 import com.example.jsconsumerloans.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "贷款申请controller", tags = {"贷款申请操作接口"})
 @RestController
 @RequestMapping("/loanapplication")
 public class LoanapplicationController {
@@ -16,8 +20,9 @@ public class LoanapplicationController {
     @Autowired
     private LoanApplication loanApplication;
 
+    @ApiOperation(value = "创建申请表单", httpMethod = "POST")
     @PostMapping("/loanapplications")
-    public Result loanApplications(@RequestBody LoansUserinfo loansUserinfo){
+    public Result loanApplications(@ApiParam(name = "loansUserinfo", value = "贷款申请表单对象json格式", required = true) @RequestBody LoansUserinfo loansUserinfo){
         return loanApplication.loanApplications(loansUserinfo);
     }
 }
